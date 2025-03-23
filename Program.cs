3c7a0ed622cb535613844f7c9219f789a376c7eb
khalishs <91864109+khalishs@users.noreply.github.com>
2025-03-23 12:30:02 +0700

    Menambahkan Class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video Details:");
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Khalish Tianto W");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
+
